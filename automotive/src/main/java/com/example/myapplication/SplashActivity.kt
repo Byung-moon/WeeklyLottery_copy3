aa5f54a54fa0fb520f64e1c2c02e07764b3780b7
Byung-moon <bm4655@naver.comn>
2024-12-01 22:52:55 +0900

    1. gif 파일 추가 2. gif 파일 재생을 위해 SplashActivity의 onCreate 부분에 로직 추가 3. 의존성 추가

--- automotive/src/main/java/com/example/myapplication/SplashActivity.kt
+++ automotive/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -4,12 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.MotionEvent
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        /////////////////////////
+        // gif 재생을 위해 코드 추가
+        val lotteryAnimationView = findViewById<ImageView>(R.id.lotteryAnimationView)
+
+        // Glide를 사용하여 GIF를 로드
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.weeklylottery_splash_animation)
+            .into(lotteryAnimationView)
+        ////////////////////////////
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
