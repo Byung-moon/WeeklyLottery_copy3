23bd209c36182481891ee42c314c8f2825fb68f4
Byung-moon <bm4655@naver.comn>
2024-12-05 20:40:41 +0900

    1. 메인 버튼 이미지 추가 2. 앱 로고 수정

--- automotive/src/main/java/com/example/myapplication/MainActivity.kt
+++ automotive/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
@@ -10,8 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val historyButton = findViewById<LinearLayout>(R.id.historyButton)
-        val gameButton = findViewById<LinearLayout>(R.id.gameButton)
+//        val historyButton = findViewById<LinearLayout>(R.id.historyButton)
+//        val gameButton = findViewById<LinearLayout>(R.id.gameButton)
+        val naviButton = findViewById<ImageButton>(R.id.naviButton)
+        val historyButton = findViewById<ImageButton>(R.id.historyButton)
+        val gameButton = findViewById<ImageButton>(R.id.gameButton)
+
+//        naviButton.setOnClickListener {
+//            val intent = Intent(this, NaviActivity::class.java)
+//            startActivity(intent)
+//        }
+
 
         historyButton.setOnClickListener {
             val intent = Intent(this, HistoryActivity::class.java)
